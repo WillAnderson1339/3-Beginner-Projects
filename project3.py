fe8f689d2857fbd844cd2ec11f94c32c60db4a09
WillAnderson1339 <WillAnderson1339@gmail.com>
2024-01-08 11:05:10 -0800

    Module 2: Corrected typos

--- project3.py
+++ project3.py
@@ -1,10 +1,10 @@
 import random
 import time
 
-OPERATORS = ["+", "-", "*"]
+OPERATORS = ["+", "-", "*", "/", "%"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
-TOTAL_PROBLEMS = 10
+TOTAL_PROBLEMS = 7
 
 
 def generate_problem():
@@ -13,13 +13,17 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expr = str(left) + " " + operator + " " + str(right)
-    answer = eval(expr)
+    answer = round(eval(expr), 1)
     return expr, answer
 
 
 wrong = 0
-input("Press enter to start!")
-print("----------------------")
+print("\nMath Quiz!")
+print("   Enter the answers as quickly as you can with as few mistakes as possible.")
+print("   This quiz uses the operators", OPERATORS)
+print("   For division round to 1 decimal place")
+input("\nPress enter to start!")
+print("------------------------")
 
 start_time = time.time()
 
@@ -34,5 +38,5 @@ for i in range(TOTAL_PROBLEMS):
 end_time = time.time()
 total_time = round(end_time - start_time, 2)
 
-print("----------------------")
-print("Nice work! You finished in", total_time, "seconds!")
+print("------------------------")
+print("Nice work! You finished in", total_time, "seconds! And you got", wrong, "incorrect.")
