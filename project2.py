0131c2d723ca08253a209b706e8930205202d025
WillAnderson1339 <WillAnderson1339@gmail.com>
2024-01-07 22:39:38 -0800

    Module 2: Finished the printing functions Cleaned up the code

--- project2.py
+++ project2.py
@@ -1,17 +1,5 @@
 from project2_utils import *
 
-def countStoryLetterFrequeny(story, vowelList, letterList):
-    # loop through the story letters counting the number of each character
-    totalLetters = len(story)
-    print(totalLetters)
-    for i, char in enumerate(story):
-        if (char.upper() in vowelList):
-            vowelList[char.upper()][1] += 1
-
-    for item in vowelList:
-        vowelList[item][1] = vowelList[item][1] / totalLetters * 100
-
-    return
 
 # open story and read contents
 with open("story.txt", "r") as f:
@@ -33,11 +21,6 @@ for i, char in enumerate(the_story):
         words.add(word)
         start_of_word = -1
 
-countStoryLetterFrequeny(the_story, vowel_frequency, letter_frequency)
-
-printLetterFrequency(vowel_frequency, letter_frequency)
-
-'''
 # for each word in the words list prompt the user for a value and store it in a dictionary
 answers = {}
 
@@ -46,7 +29,11 @@ for word in words:
     answers[word] = answer
 
 for word in words:
-    story = story.replace(word, answers[word])
+    the_story = the_story.replace(word, answers[word])
 
-print(story)
-'''
+print(the_story)
+
+countStoryLetterFrequeny(the_story, vowel_frequency, letter_frequency)
+
+printLetterFrequency(vowel_frequency, letter_frequency)
+
--- project2_utils.py
+++ project2_utils.py
@@ -1,3 +1,4 @@
+# dictionary with key = letter; and value a tuple of English letter frequency (Googled), story letter frequency (calculated)
 vowel_frequency = {
     "A": [8.167, 0],
     "E": [12.702, 0],
@@ -7,6 +8,7 @@ vowel_frequency = {
     "Y": [1.974, 0],
 }
 
+# dictionary with key = letter; and value a tuple of English letter frequency (Googled), story letter frequency (calculated)
 letter_frequency = {
     "B": [1.492, 0],
     "C": [2.782, 0],
@@ -30,61 +32,91 @@ letter_frequency = {
     "Z": [0.074, 0],
 }
 
+def countStoryLetterFrequeny(story, vowelList, letterList):
+
+    # loop through the story letters counting the number of each letter
+    totalLetters = 0
+
+    for i, char in enumerate(story):
+        char = char.upper()
+        if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            totalLetters += 1
+            if (char in vowelList):
+                vowelList[char][1] += 1
+            elif (char in letterList):
+                letterList[char][1] += 1
+
+    # divide the count by the nuber of letter to create a per centage
+    for item in vowelList:
+        vowelList[item][1] = vowelList[item][1] / totalLetters * 100
+    for item in letterList:
+        letterList[item][1] = letterList[item][1] / totalLetters * 100
+
+    return
 
 def printLetterFrequency(vowelFrequency, letterFrequency):
 
     '''
+    # unicode values from https://en.wikipedia.org/wiki/List_of_Unicode_characters
+
     symbs = [u'\u2502', u'\u2503', u'\u2550', u'\u2551', u'\u2554', u'\u2557', u'\u255a', u'\u255d', u'\u2560', u'\u2566', u'\u2567', u'\u2562', u'\u255f']
     for sym in symbs:
         print(sym)
     print("looping...")
-    '''
-    '''
     i = 0
     while i < len(symbs):
         x = hex(symbs[i])
        print(x)
         i += 1
-    '''
-    #print("╔═══════╗")
-    #print("║ A ┃ B ║")
-    #print("╟═══════╢")
-    #print("╚═══════╝")
 
-    #countStoryLetterFrequeny(vowel_frequency, letter_frequency)
+    print("╔═══════╗")
+    print("║ A ┃ B ║")
+    print("╟═══════╢")
+    print("╚═══════╝")
+    '''
+
+    printFrequency(vowelFrequency)
+    printFrequency(letterFrequency)
+
+    return
+
+def printFrequency(frequency):
 
     lineTwo    = "║ English frequency "
     lineThree  = "║ Story frequency   "
-    numCols = len(vowelFrequency)
+    numCols = len(frequency)
     numDigits = 2   # number of digits to round to
-    width = len(lineTwo) + numCols * (4 + numDigits) +4
+    width = len(lineTwo) + numCols * (5 + numDigits) +(numCols - 1)
 
-    topLine = "╔" + (width+2) * u'\u2550' + "╗"
-    bottomLine = "╚" + (width+2) * u'\u2550' + "╝"
-    headerLine = "║ Letter           "
-    middleLine = "╟" + (width+2) * u'\u2501' + "╢"
+    topLine = "╔" + (width+1) * u'\u2550' + "╗"
+    bottomLine = "╚" + (width+1) * u'\u2550' + "╝"
+    letterLine = "║ Letter          "
+    middleLine = "╟" + (width+1) * u'\u2501' + "╢"
 
 
-    for key in vowelFrequency:
-        engFrequency = '{0:.2f}'.format(vowelFrequency[key][0])
-        if vowelFrequency[key][0] < 10:
-            engFrequencyStr = str(engFrequency)
-        else:
+    for key in frequency:
+        engFrequency = '{0:.2f}'.format(frequency[key][0])
+        if frequency[key][0] < 10:
             engFrequencyStr = " " + str(engFrequency)
+        else:
+            engFrequencyStr = str(engFrequency)
 
-        storyFrequency = '{0:.2f}'.format(vowelFrequency[key][1])
+        storyFrequency = '{0:.2f}'.format(frequency[key][1])
+        if frequency[key][1] < 10:
+            storyFrequencyStr = " " + str(storyFrequency)
+        else:
+            storyFrequencyStr = str(storyFrequency)
 
-        headerLine += "  ┃   " +  key
-        #lineTwo    += " ┃ " + str(engFrequency)
+        letterLine += "   ┃   " +  key
         lineTwo    += " ┃ " + engFrequencyStr
-        lineThree  += " ┃ " + str(storyFrequency)
-    headerLine += "  ║ "
+        lineThree  += " ┃ " + storyFrequencyStr
+    letterLine += "   ║ "
     lineTwo    += " ║ "
     lineThree  += " ║ "
 
     print("\nLetter Distribution %\n")
     print(topLine)
-    print(headerLine)
+    print(letterLine)
     print(middleLine)
     print(lineTwo)
     print(middleLine)
